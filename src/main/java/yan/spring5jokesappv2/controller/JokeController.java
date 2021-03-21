afa464bd6fcf95468e2e443a197363d98eabb200
sunnycat000 <yanlily0111@gmail.com>
2021-03-21 12:58:59 -0700

    Create Controller

---
+++ src/main/java/yan/spring5jokesappv2/controller/JokeController.java
@@ -0,0 +1,23 @@
+package yan.spring5jokesappv2.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import yan.spring5jokesappv2.services.JokeService;
+
+@Controller
+public class JokeController {
+    private final JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokeService.getJoke());
+        return "index";
+    }
+}
